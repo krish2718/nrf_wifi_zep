krish2718/nrf_wifi

Add nRF71 support
nRF71 is a combo SoC that uses IPC to communicate between the APP and
Wi-Fi cores. This is a preliminary support that removes HPQM, Firmware
loading, any direct memory access to the Wi-Fi domain etc for nRF71
using a define.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

fw_if/umac_if/src/common/fmac_api_common.c
@@ -285,6 +285,7 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_chunk_load(struct nrf_wifi_fmac_dev_ctx *f
 				       fw_chunk->size);
 }
 
+#ifndef NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					   struct nrf_wifi_fmac_fw_info *fmac_fw)
 {
@@ -353,13 +354,14 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_de
 out:
 	return status;
 }
+#endif /* !NRF71_ON_IPC */
 
 
 enum nrf_wifi_status nrf_wifi_fmac_ver_get(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					  unsigned int *fw_ver)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
-
+#ifndef NRF71_ON_IPC
 	status = hal_rpu_mem_read(fmac_dev_ctx->hal_dev_ctx,
 				  fw_ver,
 				  RPU_MEM_UMAC_VER,
@@ -372,10 +374,14 @@ enum nrf_wifi_status nrf_wifi_fmac_ver_get(struct nrf_wifi_fmac_dev_ctx *fmac_de
 	}
 
 out:
+#else
+	*fw_ver = 0x01020304;
+	status = NRF_WIFI_STATUS_SUCCESS;
+#endif /* !NRF71_ON_IPC */
 	return status;
 }
 
-
+#ifndef NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 						    unsigned char vif_idx,
 						    unsigned char *mac_addr)
@@ -440,6 +446,7 @@ enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx
 out:
 	return status;
 }
+#endif /*! NRF71_ON_IPC */
 
 enum nrf_wifi_status nrf_wifi_fmac_get_reg(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					   struct nrf_wifi_fmac_reg_info *reg_info)

fw_if/umac_if/src/system/fmac_api.c
@@ -385,7 +385,9 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 					    unsigned char *country_code)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
-	struct nrf_wifi_fmac_otp_info otp_info;
+#ifndef NRF71_ON_IPC
+        struct nrf_wifi_fmac_otp_info otp_info;
+#endif /* !NRF71_ON_IPC */
 	struct nrf_wifi_phy_rf_params phy_rf_params;
 
 	if (!fmac_dev_ctx) {
@@ -413,6 +415,7 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 		goto out;
 	}
 
+#ifndef NRF71_ON_IPC
 	nrf_wifi_osal_mem_set(&otp_info,
 			      0xFF,
 			      sizeof(otp_info));
@@ -434,6 +437,7 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 				      __func__);
 		goto out;
 	}
+#endif /* !NRF71_ON_IPC */
 
 	status = nrf_wifi_sys_fmac_fw_init(fmac_dev_ctx,
 				       &phy_rf_params,

fw_if/umac_if/src/system/rx.c
@@ -177,7 +177,9 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 	struct nrf_wifi_sys_fmac_priv *sys_fpriv = NULL;
 	unsigned long nwb = 0;
 	unsigned long nwb_data = 0;
+#ifndef NRF71_ON_IPC
 	unsigned long phy_addr = 0;
+#endif /* NRF71_ON_IPC */
 	unsigned int buf_len = 0;
 
 	sys_dev_ctx = wifi_dev_priv(fmac_dev_ctx);
@@ -218,12 +220,12 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 		nwb_data = (unsigned long)nrf_wifi_osal_nbuf_data_get((void *)nwb);
 
 		*(unsigned int *)(nwb_data) = desc_id;
-
+#ifndef NRF71_ON_IPC
 		phy_addr = nrf_wifi_sys_hal_buf_map_rx(fmac_dev_ctx->hal_dev_ctx,
-						       nwb_data,
-						       buf_len,
-						       pool_info.pool_id,
-						       pool_info.buf_id);
+						   nwb_data,
+						   buf_len,
+						   pool_info.pool_id,
+						   pool_info.buf_id);
 
 		if (!phy_addr) {
 			nrf_wifi_osal_log_err("%s: nrf_wifi_sys_hal_buf_map_rx failed",
@@ -234,13 +236,15 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 
 		rx_buf_info->nwb = nwb;
 		rx_buf_info->mapped = true;
-
+#endif /* NRF71_ON_IPC */
 		nrf_wifi_osal_mem_set(&rx_cmd,
 				      0x0,
 				      sizeof(rx_cmd));
-
+#ifndef NRF71_ON_IPC
 		rx_cmd.addr = (unsigned int)phy_addr;
-
+#else
+		rx_cmd.addr = (unsigned int)nwb_data;
+#endif /* NRF71_ON_IPC */
 		status = nrf_wifi_sys_hal_data_cmd_send(fmac_dev_ctx->hal_dev_ctx,
 							NRF_WIFI_HAL_MSG_TYPE_CMD_DATA_RX,
 							&rx_cmd,
@@ -248,6 +252,7 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 							desc_id,
 							pool_info.pool_id);
 	} else if (cmd_type == NRF_WIFI_FMAC_RX_CMD_TYPE_DEINIT) {
+#ifndef NRF71_ON_IPC
 		/* TODO: Need to initialize a command and send it to LMAC
 		 * when LMAC is capable of handling deinit command
 		 */
@@ -269,10 +274,13 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 					      __func__);
 			goto out;
 		}
+#endif /* NRF71_ON_IPC */
 
 		nrf_wifi_osal_nbuf_free((void *)rx_buf_info->nwb);
+#ifndef NRF71_ON_IPC
 		rx_buf_info->nwb = 0;
 		rx_buf_info->mapped = false;
+#endif /* NRF71_ON_IPC */
 		status = NRF_WIFI_STATUS_SUCCESS;
 	} else {
 		nrf_wifi_osal_log_err("%s: Unknown cmd_type (%d)",
@@ -387,7 +395,7 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 			status = NRF_WIFI_STATUS_FAIL;
 			continue;
 		}
-
+#ifndef NRF71_ON_IPC
 		nwb_data = (void *)nrf_wifi_sys_hal_buf_unmap_rx(fmac_dev_ctx->hal_dev_ctx,
 								 pkt_len,
 								 pool_info.pool_id,
@@ -399,16 +407,24 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 			status = NRF_WIFI_STATUS_FAIL;
 			continue;
 		}
-
+#endif /* NRF71_ON_IPC */
 		rx_buf_info = &sys_dev_ctx->rx_buf_info[desc_id];
 		nwb = (void *)rx_buf_info->nwb;
 
+		/**
+		 * For Wezen the RX_BUF_HEADROOM does not seem to be needed.
+		 * Remove the same. What parameters does RX_BUF_HEADROOM have??
+		 */
+#ifndef NRF71_ON_IPC
 		nrf_wifi_osal_nbuf_data_put(nwb,
 					    pkt_len + RX_BUF_HEADROOM);
 		nrf_wifi_osal_nbuf_data_pull(nwb,
 					     RX_BUF_HEADROOM);
+#else
+		nrf_wifi_osal_nbuf_data_put(nwb,
+					    pkt_len);
+#endif
 		nwb_data = nrf_wifi_osal_nbuf_data_get(nwb);
-
 		rx_buf_info->nwb = 0;
 		rx_buf_info->mapped = false;
 

krish2718/nrf_wifi_zep

fw_if/umac_if/src/common/fmac_api_common.c
@@ -285,6 +285,7 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_chunk_load(struct nrf_wifi_fmac_dev_ctx *f
 				       fw_chunk->size);
 }
 
+#ifndef CONFIG_NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					   struct nrf_wifi_fmac_fw_info *fmac_fw)
 {
@@ -353,13 +354,14 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_de
 out:
 	return status;
 }
+#endif /* !CONFIG_NRF71_ON_IPC */
 
 
 enum nrf_wifi_status nrf_wifi_fmac_ver_get(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					  unsigned int *fw_ver)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
-
+#ifndef CONFIG_NRF71_ON_IPC
 	status = hal_rpu_mem_read(fmac_dev_ctx->hal_dev_ctx,
 				  fw_ver,
 				  RPU_MEM_UMAC_VER,
@@ -372,10 +374,14 @@ enum nrf_wifi_status nrf_wifi_fmac_ver_get(struct nrf_wifi_fmac_dev_ctx *fmac_de
 	}
 
 out:
+#else
+	*fw_ver = 0x01020304;
+	status = NRF_WIFI_STATUS_SUCCESS;
+#endif /* !CONFIG_NRF71_ON_IPC */
 	return status;
 }
 
-
+#ifndef CONFIG_NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 						    unsigned char vif_idx,
 						    unsigned char *mac_addr)
@@ -440,6 +446,7 @@ enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx
 out:
 	return status;
 }
+#endif /*! CONFIG_NRF71_ON_IPC */
 
 enum nrf_wifi_status nrf_wifi_fmac_get_reg(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					   struct nrf_wifi_fmac_reg_info *reg_info)

fw_if/umac_if/src/system/fmac_api.c
@@ -385,7 +385,9 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 					    unsigned char *country_code)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
-	struct nrf_wifi_fmac_otp_info otp_info;
+#ifndef CONFIG_NRF71_ON_IPC
+        struct nrf_wifi_fmac_otp_info otp_info;
+#endif /* !CONFIG_NRF71_ON_IPC */
 	struct nrf_wifi_phy_rf_params phy_rf_params;
 
 	if (!fmac_dev_ctx) {
@@ -413,6 +415,7 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 		goto out;
 	}
 
+#ifndef CONFIG_NRF71_ON_IPC
 	nrf_wifi_osal_mem_set(&otp_info,
 			      0xFF,
 			      sizeof(otp_info));
@@ -434,6 +437,7 @@ enum nrf_wifi_status nrf_wifi_sys_fmac_dev_init(struct nrf_wifi_fmac_dev_ctx *fm
 				      __func__);
 		goto out;
 	}
+#endif /* !CONFIG_NRF71_ON_IPC */
 
 	status = nrf_wifi_sys_fmac_fw_init(fmac_dev_ctx,
 				       &phy_rf_params,

fw_if/umac_if/src/system/rx.c
@@ -177,7 +177,9 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 	struct nrf_wifi_sys_fmac_priv *sys_fpriv = NULL;
 	unsigned long nwb = 0;
 	unsigned long nwb_data = 0;
+#ifndef CONFIG_NRF71_ON_IPC
 	unsigned long phy_addr = 0;
+#endif /* CONFIG_NRF71_ON_IPC */
 	unsigned int buf_len = 0;
 
 	sys_dev_ctx = wifi_dev_priv(fmac_dev_ctx);
@@ -218,12 +220,12 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 		nwb_data = (unsigned long)nrf_wifi_osal_nbuf_data_get((void *)nwb);
 
 		*(unsigned int *)(nwb_data) = desc_id;
-
-		phy_addr = nrf_wifi_sys_hal_buf_map_rx(fmac_dev_ctx->hal_dev_ctx,
-						       nwb_data,
-						       buf_len,
-						       pool_info.pool_id,
-						       pool_info.buf_id);
+#ifndef CONFIG_NRF71_ON_IPC
+		phy_addr = nrf_wifi_hal_buf_map_rx(fmac_dev_ctx->hal_dev_ctx,
+						   nwb_data,
+						   buf_len,
+						   pool_info.pool_id,
+						   pool_info.buf_id);
 
 		if (!phy_addr) {
 			nrf_wifi_osal_log_err("%s: nrf_wifi_sys_hal_buf_map_rx failed",
@@ -234,13 +236,15 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 
 		rx_buf_info->nwb = nwb;
 		rx_buf_info->mapped = true;
-
+#endif /* CONFIG_NRF71_ON_IPC */
 		nrf_wifi_osal_mem_set(&rx_cmd,
 				      0x0,
 				      sizeof(rx_cmd));
-
+#ifndef CONFIG_NRF71_ON_IPC
 		rx_cmd.addr = (unsigned int)phy_addr;
-
+#else
+		rx_cmd.addr = (unsigned int)nwb_data;
+#endif /* CONFIG_NRF71_ON_IPC */
 		status = nrf_wifi_sys_hal_data_cmd_send(fmac_dev_ctx->hal_dev_ctx,
 							NRF_WIFI_HAL_MSG_TYPE_CMD_DATA_RX,
 							&rx_cmd,
@@ -248,6 +252,7 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 							desc_id,
 							pool_info.pool_id);
 	} else if (cmd_type == NRF_WIFI_FMAC_RX_CMD_TYPE_DEINIT) {
+#ifndef CONFIG_NRF71_ON_IPC
 		/* TODO: Need to initialize a command and send it to LMAC
 		 * when LMAC is capable of handling deinit command
 		 */
@@ -269,10 +274,13 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_cmd_send(struct nrf_wifi_fmac_dev_ctx *fma
 					      __func__);
 			goto out;
 		}
+#endif /* CONFIG_NRF71_ON_IPC */
 
 		nrf_wifi_osal_nbuf_free((void *)rx_buf_info->nwb);
+#ifndef CONFIG_NRF71_ON_IPC
 		rx_buf_info->nwb = 0;
 		rx_buf_info->mapped = false;
+#endif /* CONFIG_NRF71_ON_IPC */
 		status = NRF_WIFI_STATUS_SUCCESS;
 	} else {
 		nrf_wifi_osal_log_err("%s: Unknown cmd_type (%d)",
@@ -387,7 +395,7 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 			status = NRF_WIFI_STATUS_FAIL;
 			continue;
 		}
-
+#ifndef CONFIG_NRF71_ON_IPC
 		nwb_data = (void *)nrf_wifi_sys_hal_buf_unmap_rx(fmac_dev_ctx->hal_dev_ctx,
 								 pkt_len,
 								 pool_info.pool_id,
@@ -399,16 +407,24 @@ enum nrf_wifi_status nrf_wifi_fmac_rx_event_process(struct nrf_wifi_fmac_dev_ctx
 			status = NRF_WIFI_STATUS_FAIL;
 			continue;
 		}
-
+#endif /* CONFIG_NRF71_ON_IPC */
 		rx_buf_info = &sys_dev_ctx->rx_buf_info[desc_id];
 		nwb = (void *)rx_buf_info->nwb;
 
+		/**
+		 * For Wezen the RX_BUF_HEADROOM does not seem to be needed.
+		 * Remove the same. What parameters does RX_BUF_HEADROOM have??
+		 */
+#ifndef CONFIG_NRF71_ON_IPC
 		nrf_wifi_osal_nbuf_data_put(nwb,
 					    pkt_len + RX_BUF_HEADROOM);
 		nrf_wifi_osal_nbuf_data_pull(nwb,
 					     RX_BUF_HEADROOM);
+#else
+		nrf_wifi_osal_nbuf_data_put(nwb,
+					    pkt_len);
+#endif
 		nwb_data = nrf_wifi_osal_nbuf_data_get(nwb);
-
 		rx_buf_info->nwb = 0;
 		rx_buf_info->mapped = false;
 